2a9e476b501af53b67e317574f09ce0f5858d36c
RAZIN M <razinm24bcs24@iiitkottayam.ac.in>
2026-01-08 15:54:44 +0530

    Added testing module to the LLM Classifier

--- LLMs/Classifier.py
+++ LLMs/Classifier.py
@@ -1,9 +1,23 @@
 from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_cred=os.getenv("OPENAI_API_KEY")
+baseurl = os.getenv("OPENAI_BASE_URL")
 class Classifier:
     temp = 1
-    llm = ChatOpenAI(model="",temperature=temp)
-    def __init__(self,temp=1):
+    llm = ChatOpenAI(model="x-ai/grok-4-fast",api_key=api_cred,base_url=baseurl,temperature=temp)
+    def __init__(self,temp):
         self.temp = temp
+    def test(self):
         res = self.llm.invoke("This is a Test of LLM please respond with Hi this LLM is ready")
-        print(res)
+        json_res = res.text()
+        if json_res == "Hi this LLM is ready":
+            return True
+        else:
+            return False
     def pass_to_LLM(self,lst):
+        pass
+smth = Classifier(1)
+print(smth.test())
+
--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
-
+from LLMs import Classifier
 router = APIRouter()
+
+LLM = Classifier
+
+
 
 @router.get("/orders")
 async  def get_orders():
