baf5e673480f74a9c830eac8fa4298333fe51b0a
RAZIN M <razinm24bcs24@iiitkottayam.ac.in>
2026-01-12 13:28:50 +0530

    Integrated LLM

--- LLMs/Classifier.py
+++ LLMs/Classifier.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,7 +18,16 @@ class Classifier:
         else:
             return False
     def pass_to_LLM(self,lst):
-        pass
-smth = Classifier(1)
-print(smth.test())
+        res = []
+        for i in lst:
+            message = [
+                {"role": "system",
+                 "content": "You are Summarizer and classifier of these Orders from LSGD website.Return in json fromat with keys title,summary,link to document,date,and a class in [Resource managment,transfer order,promotion order]"},
+                {"role": "user", "content": i}]
+            resp = self.llm.invoke(message)
+            res.append(resp)
+        print(res)
+        return resp
+
+
 
--- routes.py
+++ routes.py
@@ -5,9 +5,15 @@ RSSHandler = RSS.RSSFeed()
 router = APIRouter()
 from RSSFeedHandler import Order
 St = Order.Storage()
-
+ll = Classifier.Classifier(1)
 
 
 @router.get("/orders")
 async  def get_orders():
     return {"Res": RSSHandler.get_orders()}
+
+@router.get("/jsontest")
+async def get_json():
+    data = RSSHandler.get_orders()
+    res = ll.pass_to_LLM(data)
+    return {"Result":res}
